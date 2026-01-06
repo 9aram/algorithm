6121fad11996c648e07226ebf33541968e614aea
GARAM LEE <39611703+9aram@users.noreply.github.com>
2026-01-06 20:01:38 +0900

    [Silver IV] Title: 수 찾기, Time: 3520 ms, Memory: 71668 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -5,14 +5,12 @@ const N = Number(input[idx++]); //첫번째 줄이 저장된다.
 const mp = new Map();
 
 for (let i = 0; i < N; i++) {
-    const v = Number(input[idx++]);
     //mp.set(i,v); 인텍스의 값찾기가 목적일때. 근데 이 문제는 값 존재여부라서 x
-    mp.set(v, 1);
+    mp.set(Number(input[idx++]), 1);
 }
 
 const M = Number(input[idx++]);
 
 for (let i = 0; i < M; i++) {
-    const q = Number(input[idx++]);
-    console.log(mp.has(q) ? 1 : 0);
+    console.log(mp.has(Number(input[idx++])) ? 1 : 0);
 }
