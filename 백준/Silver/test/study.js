7ebf9ae103984ae78e9b9df4395fd4ddc7203d2f
GARAM LEE <dlrkxks1028@naver.com>
2026-01-09 23:20:12 +0900

    local sudy_stack

--- 백준/Silver/test/study.js
+++ 백준/Silver/test/study.js
@@ -1,27 +1,33 @@
-//const { listenerCount } = require("process");
 const input = require("fs").readFileSync("/Users/garam/Documents/projects/algorithm/백준/Silver/test/text.txt").toString().split(/\s+/);
 
 let index = 0;
-//const number = input[index++] 해서 이걸 다시 쪼갰더니 정규식을 잘못알고있었던것.
-const N= Number(input[index++]);
-const M = Number(input[index++]);
+const N = Number(input[index++]);
 
-const mp = new Map();
-
-//i < index + N 이렇게 했더니 인덱스가 무한늘어남 
-for(let i=0; i<N; i++){//0,3/1,3/2,3
-    mp.set(input[index++],"1");//N의 갯수만큼 돌림
+const value = [];
+for(let i=0; i<N; i++){
+    value.push(Number(input[index++]));
 }
 
-const answer=[];
-for (let j=0 ; j<M; j++){//0,4/1,4/2,4/3,4
-    const name = input[index++];
-    if(mp.has(name)){
-        
-        answer.push(name);
-    };
+const stack = [];
+stack.push(value[value.length-1]);
+let count=1;
+for (let i= N-2; i>=0;i-- ){
+    if(value[i]>stack[stack.length-1]){
+        stack.push(value[i])
+        count++
+    }
 }
-answer.sort();
-console.log(answer.length);
-console.log(answer.join("\n"));
+console.log(count);
 
+const lastVal = input[input.length-1];
+let biggerN=input[input.length-1];
+
+for(let i=0; i<N; i++){
+    const value = input[index++];
+    if(value >biggerN){
+        count++
+        biggerN=value;
+    }
+
+
+}
