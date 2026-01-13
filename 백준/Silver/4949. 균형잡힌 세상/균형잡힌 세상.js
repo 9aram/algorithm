2c2560df9fec4cac37e6889d1eea6ef987587f86
GARAM LEE <39611703+9aram@users.noreply.github.com>
2026-01-13 21:59:00 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 152 ms, Memory: 12108 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.js
@@ -0,0 +1,34 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split(/\n/); //일단 줄대로 끊는다
+
+const result=[];
+for(let line of input ){
+    if (line === ".") break; // 조건중 . 이나오면 마지막 줄이기에 종료
+    
+    const stack = [];
+    let same = true;
+    
+    for (let i=0; i<line.length; i ++){
+      if(line[i] === '(' || line[i] === '['){ //일단 여는 괄호나오면 무조건 스택에 담는다.
+        stack.push(line[i]);
+      }else if(line[i] === ')'){
+          if(stack.length === 0 || stack[stack.length-1] !=='('){
+            same = false;
+            break;
+          }
+          stack.pop();
+      }else if(line[i] === ']'){
+          if(stack.length === 0 || stack[stack.length-1]!== '['){
+            same = false;
+            break;
+          }
+          stack.pop();
+      }
+    }
+   if(stack.length>0){
+    same = false;
+   }
+   result.push(same ? "yes": "no");
+    //console.log(same ? "yes": "no");
+
+}
+console.log(result.join("\n"));
