05bf31a37093b1aeddc9c7588808e460118f65bf
GARAM LEE <39611703+9aram@users.noreply.github.com>
2026-01-19 22:06:59 +0900

    [Silver II] Title: 좌표 압축, Time: 1536 ms, Memory: 343704 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.js
@@ -0,0 +1,18 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split(/\n/);
+const N = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+//Set으로 중복을 죽이고 → [...]으로 다시 배열로 만들어서 → sort로 정렬
+const uniqueArr = [... new Set(arr)]; //...:주머니 제거, set: 중복이 제거된 객체 생성
+//arr.sort((a, b) => a - b); 오름차순정렬
+uniqueArr.sort((a,b)=>a-b);
+
+const rankMap = new Map();
+//uniqueArr 값마다 순서 저장
+uniqueArr.forEach((val,idx)=>{
+  rankMap.set(val,idx)
+});
+let result =[];
+for(let i=0; i<N; i++){
+  result.push(rankMap.get(arr[i]));
+}
+console.log(result.join(" "));
