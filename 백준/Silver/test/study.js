50c2f8505663c9dd9586dbabd7726decd7b76238
GARAM LEE <dlrkxks1028@naver.com>
2026-01-20 21:38:29 +0900

    local study

--- 백준/Silver/test/study.js
+++ 백준/Silver/test/study.js
@@ -5,35 +5,20 @@ const input = require("fs")
   .toString()
   .split(/\n/); //일단 줄대로 끊는다
 
-const result = [];
-for (let line of input) {
-  if (line === ".") break; // 조건중 . 이나오면 마지막 줄이기에 종료
+const N = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+//Set으로 중복을 죽이고 → [...]으로 다시 배열로 만들어서 → sort로 정렬
+const uniqueArr = [... new Set(arr)]; //...:주머니 제거, set: 중복이 제거된 객체 생성
+//arr.sort((a, b) => a - b); 오름차순정렬
+uniqueArr.sort((a,b)=>a-b);
 
-  const stack = [];
-  let same = true;
-
-  for (let i = 0; i < line.length; i++) {
-    if (line[i] === "(" || line[i] === "[") {
-      //일단 여는 괄호나오면 무조건 스택에 담는다.
-      stack.push(line[i]);
-    } else if (line[i] === ")") {
-      if (stack.length === 0 || stack[stack.length - 1] !== "(") {
-        same = false;
-        break;
-      }
-      stack.pop();
-    } else if (line[i] === "]") {
-      if (stack.length === 0 || stack[stack.length - 1] !== "[") {
-        same = false;
-        break;
-      }
-      stack.pop();
-    }
-  }
-  if (stack.length > 0) {
-    same = false;
-  }
-  result.push(same ? "yes" : "no");
-  //console.log(same ? "yes": "no");
+const rankMap = new Map();
+//uniqueArr 값마다 순서 저장
+uniqueArr.forEach((val,idx)=>{
+  rankMap.set(val,idx)
+});
+let result =[];
+for(let i=0; i<N; i++){
+  result.push(rankMap.get(arr[i])); //원본배열을 들고와서 등수 매기기
 }
-console.log(result.join("\n"));
+console.log(result.join(" "));
